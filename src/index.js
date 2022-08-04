fa95ed5fa2b4878acf43ac0fea9fcc3ef820f759
kingo <253104320@qq.com>
2022-08-04 11:48:14 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import x from './x.js'
 console.log('hello')
 console.log(x)
-console.log('nnn');
-console.log('nnn');
-console.log('nnn');
-console.log('nnn');
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js', //注意入口的路径问题
     output: {
         filename: '[name].[contenthash].js' //注意出口要配置hash表
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '首页',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 };
